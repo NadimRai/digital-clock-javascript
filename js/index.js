b0cb23805201273aefc6bcdae9f0394aca5360d3
NadimRai <rai_nadim@yahoo.com>
2017-04-19 00:41:55 -0700

    this scope

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
+// function onReady(){
+// 	setInterval(clock,1000);
+// 	clock();
+// }
+
+// function clock(){
+// 	var date = new Date();
+// 	var clock  = document.getElementById('clock') ;
+// 	clock.innerHTML = formatDigits(date.getHours())+":"+formatDigits(date.getMinutes())+":"+formatDigits(date.getSeconds());
+
+
+// }
+
+window.onload = onReady;
+
 function onReady(){
 	var clock = createClock('clock');
 	var clock  = createClock('clock2');
@@ -8,18 +23,21 @@ function createClock(id){
 	c.updateClock = function(){
 		var date = new Date();
 		var clock = document.getElementById(id);
-		clock.innerHTML = formatDigits(date.getHours())+ ":"+
-						  formatDigits(date.getMinutes())+ ":"+
-						  formatDigits(date.getSeconds());
+		clock.innerHTML = this.formatDigits(date.getHours())+ ":"+
+						  this.formatDigits(date.getMinutes())+ ":"+
+						  this.formatDigits(date.getSeconds());
 	};
-	setInterval(c.updateClock,1000);
+	c.formatDigits = function(val){
+		if(val <10) val = "0"+ val;
+	return val
+	};
+	setInterval(function(){
+		c.updateClock();},1000);
 	c.updateClock();
 	return c;
 }
 
 
-function formatDigits(val){
-	if(val <10) val = "0"+ val;
-	return val
-}
-window.onload = onReady;
+
+
+
