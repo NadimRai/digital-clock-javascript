f53f08829c6150c429e1af6d88bef849cf6615f2
NadimRai <rai_nadim@yahoo.com>
2017-04-19 23:42:00 -0700

    create multiple clock with constructor

--- js/index.js
+++ js/index.js
@@ -16,33 +16,41 @@
 //  	return val;
 //  }
 
-window.onload = onReady;
-
 function onReady(){
+	
+
 	var clock = new Clock('clock');
-	var clock  = new Clock('clock2');
+	var clock2 = new Clock('clock2',-420,"ETC");
+	var clock3 = new Clock('clock3',300,'X');
+
+
+
 }
 
-function Clock(id){	
-	this.updateClock = function(){
-		var date = new Date();
-		var clock = document.getElementById(id);
-		clock.innerHTML = this.formatDigits(date.getHours())+ ":"+
-						  this.formatDigits(date.getMinutes())+ ":"+
-						  this.formatDigits(date.getSeconds());
-	};
-	this.formatDigits = function(val){
-		if(val <10) val = "0"+ val;
-	return val
-	};
+function Clock(id,offset,label){
+		offset = offset || 0;
+		label = label || '';
+		var d = new Date();
+		this.offset =(offset+ d.getTimezoneOffset())*60*1000;
+		 
+		this.updateClock = function(){
+			var date = new Date();
+				date = new Date((offset+ d.getTimezoneOffset())*60*1000 + date.getTime());
+			var clock = document.getElementById(id);
+			clock.innerHTML = this.formatDigits(date.getHours()) + ":" + this.formatDigits(date.getMinutes()) +":"+ this.formatDigits(date.getSeconds()) +" "+ label ;
+		};
+
+		this.formatDigits= function(val){
+			if(val<10) val = "0" + val;
+
+			return val;
+		};
+
 	var that = this;
 	setInterval(function(){
 		that.updateClock();},1000);
-	that.updateClock();
-	
+	this.updateClock();
 }
 
 
-
-
-
+window.onload = onReady;
